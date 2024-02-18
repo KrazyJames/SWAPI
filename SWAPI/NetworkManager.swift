639656632fa83c87daf03f41e434a85af7629940
Jaime Escobar Martinez <31583280+KrazyJames@users.noreply.github.com>
2024-02-18 04:35:53 -0700

    NetworkManager actor added

---
+++ SWAPI/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  SWAPI
+//
+//  Created by Jaime Escobar on 18/02/24.
+//
+
+import Foundation
+
+actor NetworkManager {
+    static let baseURL = "https://swapi.dev/api/"
+}
