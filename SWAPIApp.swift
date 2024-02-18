fcffcbb404a3cc9a4c6b23a5ca383dd75e276738
Jaime Escobar Martinez <31583280+KrazyJames@users.noreply.github.com>
2024-02-18 03:16:04 -0700

    Initial commit

---
+++ SWAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAPIApp.swift
+//  SWAPI
+//
+//  Created by Jaime Escobar on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SWAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
